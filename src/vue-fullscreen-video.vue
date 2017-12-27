44f1ab3802a4bd0f83f46f7a770cfff2f9197439
Johnny <157781745@qq.com>
2017-12-27 10:48:14 +0800

    fix build error
    update

--- src/vue-fullscreen-video.vue
+++ src/vue-fullscreen-video.vue
@@ -1,6 +1,7 @@
 <template>
     <div ref="parent" style="width: 100%;height: 100%;overflow: hidden">
-        <video ref="video" playsinline :src="url" >
+        <video ref="video" playsinline :src="url" x5-video-player-type="h5"
+        x5-video-player-fullscreen="true" preload="auto"  >
 
         </video>
 
@@ -23,11 +24,10 @@
 * Copyright 2013-2016 Johnny chen
 */
 <script>
-    import enableInlineVideo from 'iphone-inline-video';
 
     // 注册
     export default {
-        name: 'vue-movie-clip',
+        name: 'vue-fullscreen-video',
         // 声明 props
         props: {
             url: {
@@ -36,7 +36,7 @@
             },
             align: {
                 type: String,
-                default: 'crop center'
+                default: 'crop'
             },
             loop: {
                 type: Boolean,
@@ -45,20 +45,15 @@
             autoPlay: {
                 type: Boolean,
                 default: true
-            }
-            ,
-            width: {
-                type: Number,
-                default: 640
             },
-            height: {
-                type: Number,
-                default: 320
+            scroll: {
+                type: Boolean,
+                default: false
             }
         },
         data: function () {
             return {
-                playing: false,video:null,parent:null
+                playing: false, video: null, parent: null
             }
         },
         methods: {
@@ -72,54 +67,65 @@
                 this.playing = false;
                 this.$emit('stop', this);
             },
-            updateSize:function () {
-                let width=this.parent.clientWidth;
-                let height=this.parent.clientHeight;
-                let radio=width/height;
+            updateSize: function () {
+                var width = this.parent.clientWidth;
+                var height = this.parent.clientHeight;
+                var radio = width / height;
+                var cut_w = this.video.videoWidth;
+                var cut_h = this.video.videoHeight;
+                var radio2 = cut_w / cut_h;
+                var end_w, end_h;
 
-                let cut_w=this.video.videoWidth;
-                let cut_h=this.video.videoHeight;
-                let radio2=cut_w/cut_h;
+                switch (this.align) {
+                    case 'crop':
+                        if (radio2 > radio) {
+                            end_h = height;
+                            end_w = end_h * radio2;
+                        } else {
+                            end_w = width;
+                            end_h = end_w / radio2;
+                        }
+                        break;
+                    case 'center':
+                        if (radio2 > radio) {
+                            end_w = width;
+                            end_h = end_w / radio2;
 
-
-                let end_w,end_h;
-                if(radio2>radio){
-                    end_h=height;
-                    end_w=end_h*radio2;
-                }else{
-                    end_w=width;
-                    end_h=end_w/radio2;
+                        } else {
+                            end_h = height;
+                            end_w = end_h * radio2;
+                        }
+                        break;
                 }
-                this.video.width=end_w;
-                this.video.height=end_h;
-                this.video.style.marginLeft=-(end_w-width)/2+'px';
-                this.video.style.marginTop=-(end_h-height)/2+'px';
 
-                // $image->crop($cut_w, $cut_h, intval(($image->width()-$cut_w)/2), intval(($image->height()-$cut_h)/2));
-                // console.log(this.video.videoWidth)
-
+                this.video.width = end_w;
+                this.video.height = end_h;
+                this.video.style.marginLeft = -(end_w - width) / 2 + 'px';
+                this.video.style.marginTop = -(end_h - height) / 2 + 'px';
             }
         },
 
-        watch: {},
 
         mounted: function () {
-            let self = this;
-            this.parent=this.$refs.parent;
-            this.video=this.$refs.video;
+            var self = this;
+            this.parent = this.$refs.parent;
+            this.video = this.$refs.video;
             //console.log(this.$refs.video)
             this.video.oncanplay = function () {
                 self.$emit('canplay');
                 self.updateSize();
             };
-            window.onresize=function () {
+            window.onresize = function () {
                 self.updateSize();
+            };
+            if (false===this.scroll) {
+                this.parent.setAttribute('ontouchmove', 'event.preventDefault();');
             }
 
-          // this.enableInlineVideo(this.$refs.video)
+            // this.enableInlineVideo(this.$refs.video)
             //  this.updateFrame(this.frame);
             if (this.autoPlay) {
-                setTimeout(this.play,1000);
+                this.play()
             }
 
 
